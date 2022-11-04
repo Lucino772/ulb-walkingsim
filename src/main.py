1152a19198c65f4b965c754580b2e1fa8285d43c
Antoine de Frahan <antoine.defrahan@protonmail.com>
2022-11-04 18:37:30 +0100

    refactor: ground as class

--- src/main.py
+++ src/main.py
@@ -18,28 +18,6 @@ import visualiser
 def main():
     # Non-smooth mechanics physics system
     sys = chrono.ChSystemNSC()
-    # Ground rigid body
-    ground = chrono.ChBody()
-    ground.SetBodyFixed(True)
-    # Ground collision shape
-    ground.GetCollisionModel().ClearModel()
-    ground_material = chrono.ChMaterialSurfaceNSC()
-    ground.GetCollisionModel().AddBox(
-        ground_material, 10, 0.5, 10, chrono.ChVectorD(0, -1, 0)
-    )
-    ground.GetCollisionModel().BuildModel()
-    ground.SetCollide(True)
-
-    sys.Add(ground)
-
-    # Box shape for the ground
-    boxground = chrono.ChBoxShape()
-    boxground.GetBoxGeometry().Size = chrono.ChVectorD(10, 0.5, 3)
-    boxground.SetColor(chrono.ChColor(0.5, 0.7, 0.5))
-    ground.AddVisualShape(
-        boxground, chrono.ChFrameD(chrono.ChVectorD(0, -1, 0), chrono.QUNIT)
-    )
-
     # Simulation
     visuals = visualiser.SimVisualiser(sys)
     visuals.run()
