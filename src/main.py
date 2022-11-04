440c5e221277f8223028b08e39b988952e816fca
Antoine de Frahan <antoine.defrahan@protonmail.com>
2022-11-04 17:20:50 +0100

    feat: basic ground physics and vis

--- src/main.py
+++ src/main.py
@@ -11,10 +11,36 @@ Description:
     and launches the visualiser.
 """
 
+import pychrono.core as chrono
+import pychrono.irrlicht as chronoirr
 
 def main():
-    pass
+    # Non-smooth mechanics
+    sys = chrono.ChSystemNSC()
+    mat = chrono.ChMaterialSurfaceNSC()
+    ground = chrono.ChBodyEasyBox(10, 1, 10, 100, True, True, mat)
+    ground.SetBodyFixed(True);
+    ground.SetPos(chrono.ChVectorD(0, 0, 0))
+    sys.AddBody(ground)
+    
 
+    # Create the Irrlicht visualization
+    vis = chronoirr.ChVisualSystemIrrlicht()
+    vis.AttachSystem(sys)
+    vis.SetWindowSize(1024,768)
+    vis.SetWindowTitle('3D muscle-based walking sim')
+    vis.Initialize()
+    vis.AddSkyBox()
+    vis.AddCamera(chrono.ChVectorD(0, 8 , 6))
+    vis.AddTypicalLights()
+
+    #  Run the simulation
+    while vis.Run():
+        vis.BeginScene() 
+        vis.Render()
+        vis.EndScene()
+        sys.DoStepDynamics(1e-3)
+        sys.GetCollisionSystem().Visualize(chrono.ChCollisionSystem.VIS_Shapes)
 
 if __name__ == "__main__":
     main()
