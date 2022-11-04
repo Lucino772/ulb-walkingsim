0d1ea4ec92256e1f04c9dd573a2839920647e9d4
Antoine de Frahan <antoine.defrahan@protonmail.com>
2022-11-04 16:52:57 +0100

    feat: program entry point

---
+++ src/main.py
@@ -0,0 +1,20 @@
+"""
+3D PyChrono Muscle-based walking simulator
+File: main.py
+Authors:
+    BECKER Robin-Gilles
+    BOURGEOIS Noé
+    HENRY DE FRAHAN Antoine
+    PALMISANO Luca
+Description:
+    Program entry point. Builds the physics system
+    and launches the visualiser.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
