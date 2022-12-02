f88c85dfaadd8b70497803410c40854acd74ec88
Antoine de Frahan <antoine.defrahan@protonmail.com>
2022-12-03 00:12:24 +0100

    feat(tests): half collision box for legs, allowing for rotation

--- tests.py
+++ tests.py
@@ -36,26 +36,45 @@ class TestsGenotypeToPhenotype:
 
     def build_creature_with_two_legs(self):
         # TODO establish rules for joint placement in procedural generation
+        # TODO allow penetration by reducing collision box by half
         # based on this example
-        trunk = bone.Bone((0.3, 1.0, 1.0), chrono.ChVectorD(0, 2.9, 0))
+        trunk = bone.Bone((0.3, 1.0, 1.0), chrono.ChVectorD(0, 1.9, 0))
         trunk.SetBodyFixed(True)
         self.env.Add(trunk)
         # left leg
         leg1 = bone.Bone((0.3, 1.4, 0.2), chrono.ChVectorD(0, 0.7, -0.2))
+        leg1.GetCollisionModel().ClearModel()
+        leg1.GetCollisionModel().AddEllipsoid(
+            bone.Bone.bone_material,
+            0.15,
+            0.35,
+            0.1,
+            chrono.ChVectorD(0, -0.35, 0),
+        )
+        leg1.GetCollisionModel().BuildModel()
         self.env.Add(leg1)
         mlink = chrono.ChLinkMotorRotationTorque()
         mframe = chrono.ChFrameD(chrono.ChVectorD(0, 1, -0.2))
         mlink.Initialize(trunk, leg1, mframe)
         self.env.Add(mlink)
         # The torque(time) function:
-        #  mtorquetime = chrono.ChFunction_Sine(
-        #      0, 2, 160  # phase [rad]  # frequency [Hz]
-        #  )  # amplitude [Nm]
+        mtorquetime = chrono.ChFunction_Sine(
+            0, 2, 90  # phase [rad]  # frequency [Hz]
+        )  # amplitude [Nm]
         action_a = chrono.ChFunction_Const(-30)
-        mlink.SetTorqueFunction(action_a)
+        mlink.SetTorqueFunction(mtorquetime)
 
         # right leg
         leg2 = bone.Bone((0.3, 1.4, 0.2), chrono.ChVectorD(0, 0.7, 0.2))
+        leg2.GetCollisionModel().ClearModel()
+        leg2.GetCollisionModel().AddEllipsoid(
+            bone.Bone.bone_material,
+            0.15,
+            0.35,
+            0.1,
+            chrono.ChVectorD(0, -0.35, 0),
+        )
+        leg2.GetCollisionModel().BuildModel()
         self.env.Add(leg2)
         mlink2 = chrono.ChLinkMotorRotationTorque()
         mframe2 = chrono.ChFrameD(chrono.ChVectorD(0, 1, 0.2))
